faa90eb29715d32153c62470443754059bb3048f
dhruvagar69-ops <dhruvagar69@gmail.com>
2026-03-15 16:41:42 +0530

    MazeSolver_conf added

--- MazeSolver_conf.h
+++ MazeSolver_conf.h
@@ -1,24 +1,43 @@
 #ifndef MAZESOLVER_CONF_H_
 #define MAZESOLVER_CONF_H_
 
+/***************************************
+* Parameters related to the maze
+***************************************/
+//Maze size
 #define MAZE_SIZE 8
 
+// Maze goal coordinates
+// Arrange the coordinates you want to be the goal
+// It doesn't have to be 4 coordinates
 #define MAZE_GOAL_LIST {IndexVec(1,0)}
 
-#define SEARCH_DEPTH1 1
+/****************************************
+* Parameters related to the search algorithm
+****************************************/
+// k when calculating the k shortest paths after reaching the goal
+#define SEARCH_DEPTH1
 
-#define SEARCH_DEPTH2 20
+//When the search is finished and the final route is calculated, k
+#define SEARCH_DEPTH2
 
-#define TURN90_TIME 	0.6
+//Robot's driving performance used when calculating the cost of the path
+//Time taken to navigate a block with a 90-degree turn [s]
+#define TURN90_TIME
 
-#define TURN45_TIME 	0.4
+//Time taken to navigate a block that turns at a 45-degree angle [s]
+#define TURN45_TIME
 
-#define ACCELERATION 	6.0
+//Acceleration [m/s^2]
+#define ACCELERATION
 
-#define MAX_VELOCITY 	1.0
+//Maximum speed [m/s]
+#define MAX_VELOCITY
 
-#define MIN_VELOCITY 	0.2
+//Minimum speed [m/s]
+#define MIN_VELOCITY 
 
-#define MAZE_1BLOCK_LENGTH 	0.18
+//Length of one plot [m]
+#define MAZE_1BLOCK_LENGTH 	0.25
 
-#endif
+#endif /* MAZESOLVER_CONF_H_ */
